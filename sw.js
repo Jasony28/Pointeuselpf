70b4df4f9911a4443a653b3899776ce8781755f2
Jasony28 <jasonpougin1@gmail.com>
2025-08-29 21:30:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ if (workbox) {
 
   // --- VERSION DE L'APPLICATION ---
   // DOIT être exactement le même numéro que dans app.js
-  const APP_VERSION = 'v1.2.6';
+  const APP_VERSION = 'v1.2.7';
 
   // --- MISE EN CACHE DES FICHIERS DE BASE (PRECACHING) ---
   const precacheManifest = [
@@ -68,4 +68,5 @@ if (workbox) {
 
 } else {
   console.log(`[Workbox] Le chargement a échoué.`);
+
 }
